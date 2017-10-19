26047e067878176bd9e8437ac4ac96713dd43085
adriarr <adriarr@l102c1-linux.lab.inf.uva.es>
2017-10-19 13:08:42 +0200

    El otro test

---
+++ src/bicicletoide/citybike/test/CityBikeParkingPointTest.java
@@ -0,0 +1,16 @@
+package bicicletoide.citybike.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import bicicletoide.citybike.CityBikeParkingPoint;
+
+public class CityBikeParkingPointTest {
+
+	@Test
+	public void test() {
+		CityBikeParkingPoint point = new CityBikeParkingPoint();
+	}
+
+}
