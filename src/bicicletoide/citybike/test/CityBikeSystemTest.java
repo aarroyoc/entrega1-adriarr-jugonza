19a1eda15432d0e06c3967db8aa9dce1d43dca05
adriarr <adriarr@l102c1-linux.lab.inf.uva.es>
2017-10-19 13:06:48 +0200

    JUnit 4

---
+++ src/bicicletoide/citybike/test/CityBikeSystemTest.java
@@ -0,0 +1,16 @@
+package bicicletoide.citybike.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import bicicletoide.citybike.CityBikeSystem;
+
+public class CityBikeSystemTest {
+
+	@Test
+	public void create() {
+		CityBikeSystem cbs = new CityBikeSystem();
+	}
+
+}
