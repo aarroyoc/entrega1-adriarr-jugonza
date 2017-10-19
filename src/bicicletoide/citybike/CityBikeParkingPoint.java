b6ac7248e1fcd52477c4bb3217f8e6ed1d84ac36
adriarr <adriarr@l102c1-linux.lab.inf.uva.es>
2017-10-19 12:54:10 +0200

    Borrado main

--- src/bicicletoide/citybike/CityBikeParkingPoint.java
+++ src/bicicletoide/citybike/CityBikeParkingPoint.java
@@ -1,6 +1,9 @@
 package bicicletoide.citybike;
 
+import fabricante.externo.tarjetas.*;
 
 public class CityBikeParkingPoint {
-
+	public static void main(String[] args){
+		new TarjetaMonedero("");
+	}
 }
